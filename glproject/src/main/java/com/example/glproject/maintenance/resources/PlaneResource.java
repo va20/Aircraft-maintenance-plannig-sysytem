fc3d2f738fc938f1c8fc86f6fd333cefa85048ff
DIONISIO christopher <dionisio@informatique.univ-paris-diderot.fr>
2017-02-19 20:48:47 +0100

    add new project

---
+++ glproject/src/main/java/com/example/glproject/maintenance/resources/PlaneResource.java
@@ -0,0 +1,8 @@
+package com.example.glproject.maintenance.resources;
+
+import javax.ws.rs.Path;
+
+@Path("/planes")
+public class PlaneResource {
+
+}
---
+++ glproject/src/main/java/com/example/glproject/maintenance/resources/TaskResource.java
@@ -0,0 +1,9 @@
+package com.example.glproject.maintenance.resources;
+
+import javax.ws.rs.Path;
+
+@Path("/tasks")
+public class TaskResource {
+
+	
+}
