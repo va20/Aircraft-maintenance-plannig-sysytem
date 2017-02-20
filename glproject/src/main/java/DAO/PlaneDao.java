c9b8d4bd599cb2738d2127b7d70a7e1b4e9e1a2e
AL-QAZWENY saif <alqazwen@informatique.univ-paris-diderot.fr>
2017-02-20 21:56:41 +0100

    Ajouter les methodes (update,delete,add,getAllPlanes) dans l'interface PlaneDao

--- glproject/src/main/java/DAO/PlaneDao.java
+++ glproject/src/main/java/DAO/PlaneDao.java
@@ -1,10 +1,18 @@
 package DAO;
 
-import DAOImplement.Mro;
+
+import DAOImplement.Plane;
+
+
+import java.util.List;
 
 /**
  * Created by info on 20/02/17.
  */
 public interface PlaneDao {
-
+    boolean add(Plane plane);
+    boolean update(int id);
+    boolean delete(int id);
+    List<Plane> getAllPlanes();
+    //List<Plane> ByDay(Date date);
 }
