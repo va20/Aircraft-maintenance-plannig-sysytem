437499eb73d8938b50280f516569662ff55cdf8b
DIONISIO christopher <dionisio@informatique.univ-paris-diderot.fr>
2017-03-01 16:06:38 +0100

    debug

--- glproject/src/main/java/com/example/glproject/resources/PlaneResource.java
+++ glproject/src/main/java/com/example/glproject/resources/PlaneResource.java
@@ -8,26 +8,5 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/planes")
 public class PlaneResource {
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getPlanes() {
-		return "all the planes";
-	}
-
-	@GET
-	@Path("/{type}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMPD(@PathParam("type") String type) {
-		return "mpd";
-	}
-
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public String createPlane(String s) {
-		return "flight created";
-	}
 }
