5592b32c21c2d67f9f0df4bc87c2f4cf43f4d0ae
ebourefe <ebourefevianel@yahoo.fr>
2017-03-03 17:22:27 +0100

    bouchon

--- gl.git 2/gl/src/main/java/com/example/glproject/DAO/DAO.java
+++ gl.git 2/gl/src/main/java/com/example/glproject/DAO/DAO.java
@@ -1,11 +1,31 @@
 package com.example.glproject.DAO;
 
 import com.example.glproject.DAOimpl.FlightDAOimpl;
+import com.example.glproject.DAOimpl.GenericTaskDAOimpl;
+import com.example.glproject.DAOimpl.MroDAOimpl;
+import com.example.glproject.DAOimpl.PlaneDAOimpl;
+import com.example.glproject.DAOimpl.TaskDAOimpl;
 
 public class DAO {
 
-	public static FlightDAOimpl getFlightDAO()
+	public static FlightDAO getFlightDAO()
 	{
 		return new FlightDAOimpl();
+	}
+	public static GenericTaskDAO getGenericTaskDAO()
+	{
+		return new GenericTaskDAOimpl();
+	}
+	public static MroDao getMroDAO ()
+	{
+		return new MroDAOimpl();
+	}
+	public static PlaneDAO getplaneDAO()
+	{
+		return new PlaneDAOimpl();
+	}
+	public static TaskDAO getTaskDAO()
+	{
+		return new TaskDAOimpl();
 	}
 }
