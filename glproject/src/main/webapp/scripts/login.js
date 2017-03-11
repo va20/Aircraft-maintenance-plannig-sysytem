3aa16c0762ef9b30a3c392104386e7d684c76ade
DIONISIO christopher <dionisio@informatique.univ-paris-diderot.fr>
2017-03-11 15:44:22 +0100

    added login ws + javascript file login.js

---
+++ glproject/src/main/webapp/scripts/login.js
@@ -0,0 +1,40 @@
+function isValidForm() {
+	$('#username, #password').on('input',function() {
+		if($('#username').val() != "" && $('#password').val() != "") {
+			$('#connexion').prop('disabled', true);
+			console.log("oui");
+			return true;
+		} else {
+			$('#connexion').prop('disabled', true)
+			return false;
+		}
+	});
+}
+
+$(function() {	
+	//if(isValidForm()) {
+		$("#submit").click(function() {
+			var login = $('#username').val();
+			var password = $('#password').val();
+			
+			$.ajax({
+				url : "ws/login/"+login+"/"+password,
+				type : "POST",
+				dataType : "json",
+				success : function(data) {
+					if(data) {
+						$("#span_con").html("Correct data");
+						console.log("ouiii");
+						//startSession(login);
+						//location.href="/list.html";
+					} else {
+						$("#span_con").html("Incorrect data");
+					}
+				},
+				error : function(resultat, statut, erreur) {
+							alert("Error");
+						}
+			});
+		});
+	//}
+});
