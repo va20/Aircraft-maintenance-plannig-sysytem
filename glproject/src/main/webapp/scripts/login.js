8aef4b3b11d37456331fbaa07acf7d3c83a6883b
DIONISIO christopher <dionisio@informatique.univ-paris-diderot.fr>
2017-03-16 21:14:26 +0100

    minor modif

--- glproject/src/main/webapp/scripts/login.js
+++ glproject/src/main/webapp/scripts/login.js
@@ -1,40 +1,31 @@
-function isValidForm() {
+$(function() {
 	$('#username, #password').on('input',function() {
 		if($('#username').val() != "" && $('#password').val() != "") {
-			$('#connexion').prop('disabled', true);
-			console.log("oui");
-			return true;
-		} else {
-			$('#connexion').prop('disabled', true)
-			return false;
-		}
-	});
-}
 
-$(function() {	
-	//if(isValidForm()) {
-		$("#submit").click(function() {
-			var login = $('#username').val();
-			var password = $('#password').val();
-			
-			$.ajax({
-				url : "ws/login/"+login+"/"+password,
-				type : "POST",
-				dataType : "json",
+			$("#submit").click(function() {
+				var login = $('#username').val();
+				var password = $('#password').val();
+				
+				$.ajax({
+					url : "ws/login/"+login+"/"+password,
+					type : "POST",
+					dataType : "json",
 
-				success : function(data) {
-					if(data) {
-						$("#span_con").html("Correct data");
-						console.log("ouiii");
-					} else {
-						$("#span_con").html("Incorrect data");
-					}
-				},
-
-				error : function(resultat, statut, erreur) {
-							alert("Error");
+					success : function(data) {
+						if(data) {
+							$("#span_con").html("Correct data");
+							console.log("ouiii");
+						} else {
+							$("#span_con").html("Incorrect data");
 						}
+					},
+
+					error : function(resultat, statut, erreur) {
+								alert("Error");
+							}
+				});
 			});
-		});
-	//}
+		} else {
+			console.log("remplir les champs svp");
+		}
 });
