725086379dae99f1f11e4f3a4bb3eb3bbda4508d
wangpin <wangpin000@gmail.com>
2017-03-22 15:53:56 +0100

    remplir tous les DAOImpl

--- glproject/src/main/java/com/example/glproject/DAOImpl/FlightDAOImpl.java
+++ glproject/src/main/java/com/example/glproject/DAOImpl/FlightDAOImpl.java
@@ -1,30 +1,171 @@
 package com.example.glproject.DAOImpl;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.search.SearchHit;
+
 import com.example.glproject.DAO.FlightDAO;
 import com.example.glproject.businessobjects.Flight;
+import com.example.glproject.persistence.ElasticSearchClient;
+import com.fasterxml.jackson.core.JsonGenerationException;
 
 public class FlightDAOImpl implements FlightDAO {
+	private ElasticSearchClient esc = ElasticSearchClient.getInstance();
+
+	public void add(Flight flight) {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = null;
+
+		try {
+			json = mapper.writeValueAsString(flight);
+
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		esc.getClient().prepareIndex("gl", "flight").setSource(json).get();
+	}
+
+
+	public void update(Flight flight) {
+		try {
+			esc.getClient().prepareUpdate("gl","flight", Long.toString(flight.getIdPlane()))
+			.setDoc(XContentFactory.jsonBuilder()
+			.startObject()
+			.field("arrAirport" , flight.getArrAirport())
+			.field("arrTime", flight.getArrTime())
+			.field("commercial", flight.getCommercial())
+			.field("depAirport", flight.getDepAirport())
+			.field("depTime", flight.getDepTime())
+			.field("idPlane", flight.getIdPlane())
+			.endObject()).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(long idPlane) {
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("flight");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits)			
+			esc.getClient().prepareDelete(sh.getIndex(), sh.getType(), Long.toString(idPlane)).get();
+	}
+
 
 	public Flight getFlight(String commercial) {
-		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("flight");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits) {
+			Flight flight = null;
+
+			try {
+				flight = mapper.readValue(sh.sourceAsString(), Flight.class);
+				
+				if(flight.getCommercial().equals(commercial)){
+					return flight;
+				}
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
 	public List<Flight> getFlights() {
-		// TODO Auto-generated method stub
-		return null;
+		ObjectMapper mapper = new ObjectMapper();
+		List<Flight> flights = new ArrayList<Flight>();
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("flight");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits) {
+			Flight flight = null;
+
+			try {
+				flight = mapper.readValue(sh.sourceAsString(), Flight.class);
+				flights.add(flight);
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return flights;
 	}
 
-	public void add(Flight flight) {
-		// TODO Auto-generated method stub
+	public List<Flight> getByDayDep(Date day) {
+		ObjectMapper mapper = new ObjectMapper();
+		List<Flight> flights = new ArrayList<Flight>();
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("flight");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits) {
+			Flight flight = null;
+
+			try {
+				flight = mapper.readValue(sh.sourceAsString(), Flight.class);
+				if(flight.getDepTime()==day)
+					flights.add(flight);
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return flights;
 	}
 
-	public List<Flight> getByDay(Date day) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Flight> getByDayArr(Date day) {
+		ObjectMapper mapper = new ObjectMapper();
+		List<Flight> flights = new ArrayList<Flight>();
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("flight");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits) {
+			Flight flight = null;
+
+			try {
+				flight = mapper.readValue(sh.sourceAsString(), Flight.class);
+				if(flight.getArrTime()==day)
+					flights.add(flight);
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return flights;
 	}
 
 }
--- glproject/src/main/java/com/example/glproject/DAOImpl/StaffDAOImpl.java
+++ glproject/src/main/java/com/example/glproject/DAOImpl/StaffDAOImpl.java
@@ -9,9 +9,11 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.search.SearchHit;
 
 import com.example.glproject.DAO.StaffDAO;
+import com.example.glproject.businessobjects.Plane;
 import com.example.glproject.businessobjects.Staff;
 import com.example.glproject.persistence.ElasticSearchClient;
 import com.fasterxml.jackson.core.JsonGenerationException;
@@ -20,6 +22,31 @@ public class StaffDAOImpl implements StaffDAO {
 	private ElasticSearchClient esc = ElasticSearchClient.getInstance();
 
 	public Staff getStaff(long id) {
+		ObjectMapper mapper = new ObjectMapper();
+
+		// query
+		SearchRequestBuilder requestBuilder = esc.getClient().prepareSearch("gl").setTypes("staff");
+		SearchResponse searchResponse = requestBuilder.get();
+		SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+		for (SearchHit sh : searchHits) {
+			Staff staff = null;
+
+			try {
+				
+				staff = mapper.readValue(sh.sourceAsString(), Staff.class);
+				if(staff.getId()==id)
+					return staff;
+
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return null;
 	}
 
@@ -79,5 +106,23 @@ public class StaffDAOImpl implements StaffDAO {
 		for (SearchHit sh : searchHits)
 			//deleting one by one
 			esc.getClient().prepareDelete(sh.getIndex(), sh.getType(), sh.getId()).get();
+	}
+
+	public void update(Staff staff) {
+		// TODO Auto-generated method stub
+		try {
+			esc.getClient().prepareUpdate("gl","staff", Long.toString(staff.getId()))
+			.setDoc(XContentFactory.jsonBuilder()
+			.startObject()
+			.field("id" ,staff.getId() )
+			.field("firstname" ,staff.getFirstname() )
+			.field("lastname" ,staff.getLastname() )
+			.field("login" ,staff.getLogin() )
+			.field("password" ,staff.getPassword() )
+			.endObject()).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
