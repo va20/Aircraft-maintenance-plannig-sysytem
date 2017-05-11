772628804c98a1c64524ca3cc626a4304b6a291c
LE LANN hugues <lelann@informatique.univ-paris-diderot.fr>
2017-05-11 13:19:26 +0200

    avancement application

---
+++ glproject/src/main/webapp/scripts/tasks_mro.js
@@ -0,0 +1,52 @@
+var tab = {};
+
+function getPlane(id) {
+	$.ajax({
+		url : "ws/planes/" + id,
+		type : "GET",
+		dataType : "json"
+	}).done(function(data) {
+		$("." + id).html(data.tailNumber);
+
+		// for plane filter
+		$(".tasks").attr("data-plane", data.tailNumber);
+	});
+}
+
+function getTasks() {
+	$.ajax({
+		url : "ws/tasks",
+		type : "GET",
+		dataType : "json"
+	}).done(
+			function(data) {
+				console.log(data);
+				printTasks(data);
+			});
+}
+
+function printTasks(data) {
+	var template = _.template($("#list_tasks").html());
+	var task = template({
+		"item" : data
+	});
+
+	// Plane tailNumber
+	_.each(data, function(item) {
+		getPlane(item.idPlane);
+	});
+
+	$("#tasks").append(task);
+
+	_.each(data, function(task) {
+		if (task.warning == "ORANGE")
+			$("#" + task.id).css("background-color", "rgba(255, 110, 0, 0.4)");
+		else if (task.warning == "RED")
+			$("#" + task.id).css("background-color", "rgba(255, 0, 0, 0.4)");
+	});
+}
+
+$(document).ready(
+		function() {
+			getTasks();
+			});
