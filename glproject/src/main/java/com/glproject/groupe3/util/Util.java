15de0875dad5f36b9454ea5d5ceb9e66ad7b4130
dionisio <cdionisio75@gmail.com>
2017-05-16 13:14:56 +0200

    sending mail alert begin

--- glproject/src/main/java/com/glproject/groupe3/util/Util.java
+++ glproject/src/main/java/com/glproject/groupe3/util/Util.java
@@ -1,7 +1,17 @@
 package com.glproject.groupe3.util;
 
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -29,5 +39,43 @@ public class Util {
 		salt = saltStringB.toString();
 
 		return salt;
+	}
+
+	public static void mail(String message, String subject) {
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		InternetAddress from = null;
+
+		try {
+			from = new InternetAddress("genielogiciel2017@gmail.com", "MCC");
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		try {
+
+			Session sess = Session.getInstance(props, new javax.mail.Authenticator() {
+				public PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("genielogiciel2017@gmail.com", "mccmrogenie");
+				}
+			});
+
+			Message m = new MimeMessage(sess);
+			m.setFrom(from);
+			m.setRecipients(Message.RecipientType.TO, InternetAddress.parse("genielogiciel2017@gmail.com"));
+			m.setSubject(subject);
+			m.setText(message);
+
+			Transport.send(m);
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 }
